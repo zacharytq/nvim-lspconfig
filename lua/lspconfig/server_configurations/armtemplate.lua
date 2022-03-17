4e9d4e428d79c3999bf10ed36c175b446f9fbe2a
Zachary Quinn <zacharytq@gmail.com>
2022-03-17 18:30:52 -0400

    shit

--- lua/lspconfig/server_configurations/armtemplate.lua
+++ lua/lspconfig/server_configurations/armtemplate.lua
@@ -2,13 +2,11 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
+    cmd = { 'dotnet /home/ztq/src/tmp/msazurermtools.azurerm-vscode-tools-0.15.6/languageServer/Microsoft.ArmLanguageServer.dll' }
     filetypes = { 'json' },
     root_dir = function(fname)
+      return util.root_pattern('.git')(fname)
+    end,
     single_file_support = true,
-  },
-
-    default_config = {
-      root_dir = [[root_pattern(".git")]],
-    },
-  },
+  }
 }
